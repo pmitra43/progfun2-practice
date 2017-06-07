dd3792be23d049b142e67117dd60642ccfbbbcb5
Priyam Mitra <mpriyam@thoughtworks.com>
2017-06-07 21:34:25 +0530

    Implementation of Var class

--- src/main/scala/week4/frp/Var.scala
+++ src/main/scala/week4/frp/Var.scala
@@ -4,7 +4,7 @@ package week4.frp
   * Created by priyammitra on 6/6/17.
   */
 class Var[T](expr: => T) extends Signal[T](expr) {
-  def update(expr: => T): Unit = ???
+  override def update(expr: => T): Unit = super.update(expr)
 }
 
 object Var {
